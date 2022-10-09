7753127aa35e17cfd3f96f7cf1281552388f7ae1
Erik Bakker <e.e.bakker@online.nl>
2022-10-09 20:31:21 +0200

    1st commit after pull from github

---
+++ Core/Inc/KeyPadConfig.h
@@ -0,0 +1,42 @@
+#ifndef	_KEYPADCONFIG_H
+#define	_KEYPADCONFIG_H
+#include <main.h>
+
+#define           _KEYPAD_DEBOUNCE_TIME_MS        20
+#define           _KEYPAD_USE_FREERTOS            0
+
+const GPIO_TypeDef* _KEYPAD_COLUMN_GPIO_PORT[] =
+{
+	COL1_GPIO_Port,
+	COL2_GPIO_Port,
+	COL3_GPIO_Port,
+	COL4_GPIO_Port
+};
+
+const uint16_t _KEYPAD_COLUMN_GPIO_PIN[] =
+{
+	COL1_Pin,
+	COL2_Pin,
+	COL3_Pin,
+	COL4_Pin
+};
+
+const GPIO_TypeDef* _KEYPAD_ROW_GPIO_PORT[] =
+{
+	ROW1_GPIO_Port,
+	ROW2_GPIO_Port,
+	ROW3_GPIO_Port,
+	ROW4_GPIO_Port,
+	ROW5_GPIO_Port
+};
+
+const uint16_t _KEYPAD_ROW_GPIO_PIN[] =
+{
+  ROW1_Pin,
+  ROW2_Pin,
+  ROW3_Pin,
+  ROW4_Pin,
+  ROW5_Pin,
+};
+
+#endif
